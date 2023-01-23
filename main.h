39fb00c579a1bd96ba9c772ba8866103d956edd1
kingbascee <kingbascee@gmail.com>
2023-01-23 03:40:39 -0800

    updated main

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -115,5 +114,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-
-#endif
+#endif /* MAIN_H */
